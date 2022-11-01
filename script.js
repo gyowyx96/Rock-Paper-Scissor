d66744776131fa4b0df9a529d80e535a029ba297
gyowyx96 <116294748+gyowyx96@users.noreply.github.com>
2022-11-01 13:18:25 +0100

    modify the whole format to implement an ui

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
+let playerScore = 0; /*setting the scoreboard*/
+let pcScore = 0;
+const btn = document.querySelectorAll("button");
 
+const scoreboard = document.createElement("div");
+scoreboard.classList.add("score");
+scoreboard.textContent= "SCOREBOARD:  ";
+const br = document.createElement("h4");
+br.textContent = "Pc: "+(pcScore);
+scoreboard.appendChild(br);
+const br1 = document.createElement("h4");
+br1.textContent = "Player: "+(playerScore);
+scoreboard.appendChild(br1);
+
+const body = document.querySelector("body");
+body.appendChild(scoreboard);
+
+// ******GAME******//
+
+btn.forEach((btn) => { //function that transform a button click 
+    btn.addEventListener('click', () => {
+        console.log(btn.id);
+        const player = (btn.id); //into a player choice
+        playRound(player);//and play a round
+
+    });
+});  
 
 function getComputerChoice(){  
     let arr = [ "rock", "scissor", "paper"];/* create an array with the output we want to have*/
@@ -6,79 +32,70 @@ function getComputerChoice(){
     return pcOut;
  }
 
-function restart(){
-    playerSelection(); /*replay the choice function*/
-}
-
-
- function playerSelection(){                
-    let playerSelection = prompt("Insert your selection");    /*ask the user to input his choice*/
-    let player = playerSelection.toLowerCase();               /*transform the string all in lowercase */ 
-    if((player === "rock") || (player === "paper") || (player === "scissor")){  /*check if the entered choice is good*/
-    return player;
-    }
-    else{
-        alert("enter a valid choice!"); /*if not let the user enter another choice*/
-        restart(); 
-    }
-}
 let result = "";
-function playRound(){
+function playRound(player){
     const pc = getComputerChoice();
-    const player = playerSelection();
-    
-    console.log("pc:" + pc);
     
     if(pc===player){ /*if we give the same input as the pc it's a draw else it start the calculation */
         let result = "draw";
+        console.log("it's a draw");
         return (result);
     }
     else{
         switch (player){ /*compare our input with the pc output and choose the winner*/
-            case "rock": pc === "paper" ? result = "loss"  : result = "win"; 
+            case "rock": pc === "paper" ? (result = "loss", console.log("rock is beaten by paper you suck!"))  
+                                        : (result = "win", console.log("you won against scissor! nice")); 
             return (result);
             break;
         
-            case "paper": pc === "scissor" ? result = "loss"  : result = "win"; 
+            case "paper": pc === "scissor" ? (result = "loss", console.log("paper loose against scissor! defeat"))  
+                                           : (result = "win", console.log("paper beat rock nice,you won!")); 
             return (result);
             break;
 
-            case "scissor": pc === "rock" ? result = "loss"  : result = "win"; 
+            case "scissor": pc === "rock" ? (result = "loss", console.log("scissor loose against rock, you are retarded!"))  
+                                          : (result = "win", console.log("scissor cut paper you won!")); 
             return (result);
             break;    
         }
     } 
 }  
-let playerScore = 0; /*setting the scoreboard*/
-let pcScore = 0;
 
 function game(){ /*the game function*/
     for(let i = 0; i<5;i++){ /*5 round game*/
-        playRound();
         if (result === "win"){ /*if you win*/
-            playerScore++; /*increase the player score*/
-            console.log("You win!");            
+            playerScore++; /*increase the player score*/           
         }
         else if (result === "loss"){
             pcScore++;
-            console.log("you lose!");
         }
-        else{
-            console.log("it's a draw!");
-        }
+        else  {
+            pcScore++;
+            playerScore++;
+        }        
     }
-    if (playerScore>pcScore){
-        console.log("pc:"+pcScore, "player:"+playerScore);
-        console.log("you won!");
+    endGame();    
+}
+
+
+
+function endGame(){
+    if (playerScore > pcScore){
+        console.log(("pc:"+pcScore), ("player:"+playerScore));
+        return console.log("you won!");
 
     }
-    else if (playerScore<pcScore){
-        console.log("pc:"+pcScore, "player:"+playerScore);
+    else if (playerScore < pcScore){
+        console.log(("pc:" + pcScore), ("player:"+playerScore));
         console.log("you loss!");
     }
-    else{
-        console.log("it's a draw!");
-    }
-    
 }
-game();
+
+
+
+
+
+
+
+
+
